4f8007258a31d4ba9d44e54bd12735d2babe6de3
Jaloliddin <alisherovthe@gmail.com>
2026-07-13 14:48:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-# .env faylini yuklash
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBAPP_URL = os.getenv("WEBAPP_URL")
 
-# Vergul bilan ajratilgan admin ID larini ro'yxatga (list) aylantiramiz
 admin_ids_str = os.getenv("ADMIN_IDS", "")
 ADMIN_IDS = [int(admin_id.strip()) for admin_id in admin_ids_str.split(",") if admin_id.strip()]
 
 if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN topilmadi! .env faylini tekshiring.")
+    raise ValueError("BOT_TOKEN topilmadi!")
